4904b93fd38f62b2f23dbe9636efd7e787199c78
Gayathiri <63051795+GAYATHIRI31@users.noreply.github.com>
2026-05-10 15:32:43 +0530

    Update personal_bot.py

--- personal_bot.py
+++ personal_bot.py
@@ -75,7 +75,7 @@ window.addEventListener("beforeunload", function (e) {
 """, unsafe_allow_html=True)
 
 # --- Handle tab close ping ---
-query_params = st.experimental_get_query_params()
+query_params = st.query_params
 if "out" in query_params:
     send_telegram("📤 He is out of chat now")
     st.stop()
